51f5512bbd5a30b3e84b4b4ec13fa027339e8a31
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-23 17:23:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown(secs)
+  while secs > 0
+    puts "#{secs} SECOND(S)!"
+    secs -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+#Can use this code syntax as well
+#def countdown(secs)
+  #until secs == 0
+    #puts "#{secs} SECOND(S)!"
+    #secs -= 1
+  #end
+  #"HAPPY NEW YEAR!"
+#end
+
+def countdown_with_sleep(secs)
+  while secs <= 5
+    puts "#{secs} SECOND(S)!"
+    secs -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
